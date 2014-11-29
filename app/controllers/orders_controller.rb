a9d65088077e9f4cba48588d4e42e26a6624f4d9
Justin Vrooman <vroomanj@gmail.com>
2014-11-28 21:33:38 -0500

    Transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,6 +41,12 @@ class OrdersController < ApplicationController
       flash[:danger] = e.message
     end
 
+    transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
+
     # @order.save
     # respond_with(@order)
     respond_to do |format|
